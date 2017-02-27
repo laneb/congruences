a6674a1ebb25505a979d7b947307159ab5944fed
laneb <lane.barlow@gmail.com>
2017-02-27 01:44:39 -0800

    update congruences header

--- congruences.h
+++ congruences.h
@@ -1,6 +1,6 @@
 #ifndef H_CONGRUENCES
 #define H_CONGRUENCES
-int chinese_remainder_solution(int numOfEquations, int long[], long mods[]);
-int * solve_congruence(int funcDegree, long funcCoeffs[], long mod);
-int * brute_force_congruence(int degree, long coeffs[], long primeMod);
+long chinese_remainder_solution(int numOfEquations, int long[], long mods[]);
+long * solve_congruence(int funcDegree, long funcCoeffs[], long mod);
+long * brute_force_congruence(int degree, long coeffs[], long primeMod);
 #endif
